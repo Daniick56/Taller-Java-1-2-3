a7e3734d7794ef0f00b8251b6564c9d0f0b35476
Daniick56 <danielballesteros22@outlook.com>
2024-05-03 14:16:39 -0500

    añadiendo algo que faltó

--- taller_3/src/Respuesta1.java
+++ taller_3/src/Respuesta1.java
@@ -1,2 +1,16 @@
+import javax.swing.*;
+
 public class Respuesta1 {
+    public static void main(String[]args) {
+        final double Descuento= 0.10;
+
+        double valorCompra=Double.parseDouble(JOptionPane.showInputDialog("Ingrese el valor de su compra"));
+
+        if(valorCompra>50000) {
+            JOptionPane.showMessageDialog(null,"Su compra es mayor de $50.000 tiene un descuento del 10% por lo tanto su total es: "+ (valorCompra-(valorCompra*0.10)));
+        }else {
+            JOptionPane.showMessageDialog(null,"El valor de su compra es :"+ valorCompra);
+        }
+
+    }
 }
